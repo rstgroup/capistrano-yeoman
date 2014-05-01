1f90331c9cb2bd39dcbeeca5078f48cd089e2c9d
Wojtek Krysiak <wojciech.krysiak@kmpgroup.pl>
2014-05-01 14:31:03 +0200

    first commit

---
+++ lib/capistrano/yeoman.rb
@@ -0,0 +1,7 @@
+require "capistrano/yeoman/version"
+
+module Capistrano
+  module Yeoman
+    # Your code goes here...
+  end
+end
---
+++ lib/capistrano/yeoman/version.rb
@@ -0,0 +1,5 @@
+module Capistrano
+  module Yeoman
+    VERSION = "0.0.1"
+  end
+end
